3145811d06a08593d9d4b8897356dc2f3f4cb8ed
ayman khallaf <engayman.khalaf@gmail.com>
2025-01-09 17:11:29 +0100

    result fetch ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-import { Destinations } from './components/result/Destination';
 import {Search} from './components/search/Search';
 
 function App() {
   return (
     <div className="App">
       <Search />
-      <Destinations />
+
+
     </div>
   );
 }
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,38 +2,37 @@ import React, { useState } from 'react';
 import { TemperatureSearch } from './Temperature';
 import { Activity } from './Activity';
 import { TravelDate } from './TravelDate';
+import { Destination } from '../result/Destination';
 
 export function Search() {
     const [temperature, setTemperature] = useState(25);
     const [activity, setActivity] = useState("beach");
     const [traveldate, setTravelDate] = useState();
-    const [destinations] = useState( [
-        {
-          id: 1,
-          name: 'Phuket',
-          country: 'Thailand',
-          temperature: 27,
-          weather: 'Sunny',
-          activities: ['Patong Beach', 'Phi Phi Islands'],
-        },
-        {
-          id: 2,
-          name: 'Canary Islands',
-          country: 'Spain',
-          temperature: 22,
-          weather: 'Partly Cloudy',
-          activities: ['Playa de las Canteras', 'Maspalomas Dunes'],
-        },
-        {
-          id: 3,
-          name: 'Maui',
-          country: 'Hawaii',
-          temperature: 25,
-          weather: 'Sunny',
-          activities: ['Wailea Beach', 'Road to Hana'],
-        },
-      ]);
-
+    const [destinations, setDestinations] = useState([{
+        id: 1,
+        name: 'Phuket',
+        country: 'Thailand',
+        temperature: 27,
+        weather: 'Sunny',
+        activities: ['Patong Beach', 'Phi Phi Islands'],
+    },
+    {
+        id: 2,
+        name: 'Canary Islands',
+        country: 'Spain',
+        temperature: 22,
+        weather: 'Partly Cloudy',
+        activities: ['Playa de las Canteras', 'Maspalomas Dunes'],
+    },
+    {
+        id: 3,
+        name: 'Maui',
+        country: 'Hawaii',
+        temperature: 25,
+        weather: 'Sunny',
+        activities: ['Wailea Beach', 'Road to Hana'],
+    },]);
+     
 
     const handleSearch = (e) => {
         e.preventDefault();
@@ -41,7 +40,7 @@ export function Search() {
         console.log(`Activity: ${activity}`);
         console.log(`Travel Date: ${traveldate}`);
     };
- 
+
     return (
         <div>
             <form>
@@ -51,7 +50,12 @@ export function Search() {
                 <TravelDate traveldate={traveldate} setTravelDate={setTravelDate} />
                 <button onClick={handleSearch}>Search</button>
             </form>
-            <destinations destinations={destinations} />
+            <ul>
+                {destinations.map((destination) => (
+                    <Destination key={destination.id} destination={destination} />
+                ))
+                }
+            </ul>
         </div>
     );
 }
