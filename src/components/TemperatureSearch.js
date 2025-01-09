84d563ff664719bcd94714127d9068371257982c
ayman khallaf <engayman.khalaf@gmail.com>
2025-01-09 13:52:21 +0100

    create temperature search components

---
+++ src/components/TemperatureSearch.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+
+
+export function TemperatureSearch() {
+  const [temperature, setTemperature] = useState(25);
+
+    return (<div>
+        <label>Preferred Temperature (°C): </label>
+        <input
+          type="number"
+          value={temperature}
+          onChange={(e) => setTemperature(e.target.value)}
+        />
+      </div>);
+}
