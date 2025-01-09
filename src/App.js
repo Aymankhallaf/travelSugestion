bb95fc8894d8770f4b7e9decef2cc74aea986d80
ayman khallaf <engayman.khalaf@gmail.com>
2025-01-09 16:12:27 +0100

    destinations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import { Card } from './components/result/Destinations';
 import {Search} from './components/search/Search';
 
 function App() {
   return (
     <div className="App">
       <Search />
-
+      <Card />
     </div>
   );
 }
