d997c53ae7a24ce4d037f609e8e1cd186ca9d039
ayman khallaf <engayman.khalaf@gmail.com>
2025-01-10 10:06:39 +0100

    use php as a backend

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,6 +3,8 @@ import { TemperatureSearch } from './Temperature';
 import { Activity } from './Activity';
 import { TravelDate } from './TravelDate';
 import { Destination } from '../result/Destination';
+import { fetchDestinations } from '../api/activity';
+import { fetchWeather } from '../api/weather';
 
 export function Search() {
     const [temperature, setTemperature] = useState(25);
@@ -32,30 +34,47 @@ export function Search() {
         weather: 'Sunny',
         activities: ['Wailea Beach', 'Road to Hana'],
     },]);
-     
 
-    const handleSearch = (e) => {
+
+    const handleSearch = async (e) => {
         e.preventDefault();
         console.log(`Temperature: ${temperature}`);
         console.log(`Activity: ${activity}`);
         console.log(`Travel Date: ${traveldate}`);
-    };
+        try {
+            const destinations2 = await fetchDestinations(activity);
+            console.log(destinations2);
+        }
+        catch(error) {
+            console.error('Error fetching destinations:', error);
 
-    return (
-        <div>
-            <form>
-                <label>Search: </label>
-                <TemperatureSearch temperature={temperature} setTemperature={setTemperature} />
-                <Activity activity={activity} setActivity={setActivity} />
-                <TravelDate traveldate={traveldate} setTravelDate={setTravelDate} />
-                <button onClick={handleSearch}>Search</button>
-            </form>
-            <ul>
-                {destinations.map((destination) => (
-                    <Destination key={destination.id} destination={destination} />
-                ))
-                }
-            </ul>
-        </div>
-    );
-}
+        }; 
+        // try {
+        //         const weather = await fetchWeather("paris", '2025-01-10');
+        //         console.log(weather);
+        //     }
+        //     catch(error) {
+        //         console.error('Error fetching weather:', error);
+    
+        //     }; 
+        
+    }
+
+        return (
+            <div>
+                <form>
+                    <label>Search: </label>
+                    <TemperatureSearch temperature={temperature} setTemperature={setTemperature} />
+                    <Activity activity={activity} setActivity={setActivity} />
+                    <TravelDate traveldate={traveldate} setTravelDate={setTravelDate} />
+                    <button onClick={handleSearch}>Search</button>
+                </form>
+                <ul>
+                    {destinations.map((destination) => (
+                        <Destination key={destination.id} destination={destination} />
+                    ))
+                    }
+                </ul>
+            </div>
+        );
+    }
