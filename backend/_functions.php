39bfd01ef01d6d9c1e803cdbe275cb42962f3851
ayman khallaf <engayman.khalaf@gmail.com>
2025-01-14 13:52:10 +0100

    func to get data from tripadvisor ok

--- backend/_functions.php
+++ backend/_functions.php
@@ -1,1 +1,24 @@
 <?php
+
+
+
+/**
+ * Get data from the OpenWeatherMap API
+ * 
+ * @param string $city
+ * @return string
+ */
+function getTripadvisorData(string $searchQuery)
+{
+    try {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', 'https://api.content.tripadvisor.com/api/v1/location/search?key=' . $_ENV["ApiTripadvisor"] . '&searchQuery=' . $searchQuery . '&language=en', [
+            'headers' => [
+                'accept' => 'application/json',
+            ],
+        ]);
+        return $response->getBody();
+    } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+        return $e->getMessage();
+    }
+}
