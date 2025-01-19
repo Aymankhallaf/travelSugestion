f5a6e661d37f342a92ed7f5db0e8bf6a5b980489
ayman khallaf <engayman.khalaf@gmail.com>
2025-01-19 17:40:44 +0100

    style travel date field

--- frontend/src/components/search/Temperature.js
+++ frontend/src/components/search/Temperature.js
@@ -9,7 +9,8 @@ export function TemperatureSearch({ temperature, setTemperature }) {
   return (
     <div className='flex-1 block overflow-hidden bg-white border-gray-200 rounded-lg	px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600'>
       <label className=''>Preferred Temperature (°C): </label>
-      <input className='mt-1 w-full border-none p-0'
+      <input
+        className='mt-1 w-full border-none p-0'
         type="number"
         value={temperature}
         onChange={(e) => {
--- frontend/src/components/search/TravelDate.js
+++ frontend/src/components/search/TravelDate.js
@@ -5,15 +5,15 @@ export function TravelDate({ travelDate, setTravelDate }) {
 
 
     return (
-        <div className='flex-1'>
+        <div className='flex-1 block overflow-hidden bg-white border-gray-200 rounded-lg	px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600'>
             <label htmlFor="travel-date">Select Travel Date: </label>
             <input
+                className='mt-1 w-full border-none p-0'
                 type="date"
                 id="travel-date"
                 value={travelDate}
                 onChange={(e) => setTravelDate(e.target.value)}
             />
-            <p>Selected Date: {travelDate}</p>
         </div>
     );
 }
