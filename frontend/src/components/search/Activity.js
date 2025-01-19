964d5405cc717835084e3d0782a58606c3c62acb
ayman khallaf <engayman.khalaf@gmail.com>
2025-01-19 17:42:12 +0100

    style Activity field

--- frontend/src/components/search/Activity.js
+++ frontend/src/components/search/Activity.js
@@ -5,12 +5,14 @@ export function Activity({ activity, setActivity }) {
 
     return (
 
-        <div className="flex-1">
-            <label className="bg-yellow-400 text-lg">Activity: </label>
-            <select value={activity} onChange={(e) => {
-                setActivity(e.target.value);
-            }
-            }>
+        <div className='flex-1 block overflow-hidden bg-white border-gray-200 rounded-lg	px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600'>
+            <label className="">Activity: </label>
+            <select
+                className='mt-1 w-full border-none p-0'
+                value={activity} onChange={(e) => {
+                    setActivity(e.target.value);
+                }
+                }>
                 <option value="beach">Beach</option>
                 <option value="beach">Family & friends</option>
                 <option value="hiking">Hiking</option>
