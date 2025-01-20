cc6b13d4ce09d0dc38a2e40f22a3a97aa7ef20bd
ayman khallaf <engayman.khalaf@gmail.com>
2025-01-20 10:01:54 +0100

    activity ok

--- frontend/src/components/search/Activity.js
+++ frontend/src/components/search/Activity.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaHiking } from 'react-icons/fa';
+import { FaRunning } from 'react-icons/fa';
 
 export function Activity({ activity, setActivity }) {
 
@@ -8,20 +8,23 @@ export function Activity({ activity, setActivity }) {
 
         <div className="flex-1 relative">
             <label chtmlFor="travel-date" className="sr-only">Activity</label>
-            <select
-                id="activity"
-                className="mt-1 block w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
-                value={activity}
-                onChange={(e) => setActivity(e.target.value)}
-                aria-label="Select activity"
-            >
-                <option value="beach">Beach</option>
-                <option value="family">Family & Friends</option>
-                <option value="hiking">Hiking</option>
-                <option value="skiing">Skiing</option>
-                <option value="cultural">Cultural Tours</option>
-            </select>
-        </div>
+            <div className="relative">
+                <FaRunning className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+
+                <select
+                    id="activity"
+                    className="mt-1 block w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
+                    value={activity}
+                    onChange={(e) => setActivity(e.target.value)}
+                    aria-label="Select activity"
+                >
+                    <option value="beach">Beach</option>
+                    <option value="family">Family & Friends</option>
+                    <option value="hiking">Hiking</option>
+                    <option value="skiing">Skiing</option>
+                    <option value="cultural">Cultural Tours</option>
+                </select>
+            </div></div>
     )
 
 }
