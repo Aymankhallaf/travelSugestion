52bd8327ff4245cbacf1b6fe89c89e0baf8acf1c
ayman khallaf <engayman.khalaf@gmail.com>
2025-01-24 15:57:10 +0100

    removing doublicated files

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Serve React app
-if (file_exists(__DIR__ . '/../frontend/build/index.html')) {
-    include __DIR__ . '/../frontend/build/index.html';
-} else {
-    echo 'React app not found.';
-}
-?>
